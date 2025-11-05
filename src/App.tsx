09b37160c82e08cff371d914f9c63901c0d82c86
João Pedro <jp.souza.dias@gmail.com>
2025-11-05 12:38:26 -0300

    importando e usando arquivos de CSS globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
+import './styles/theme.css';
+import './styles/global.css';
+
 export function App() {
-  console.log('Oi');
-  return (
-    <>
-      <h1>Olá Mundo!</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam delectus
-        eaque provident, magni harum dolores! Veritatis delectus nam commodi at
-        eligendi tempora voluptatem, eum asperiores sint, consequatur dolorum
-        repellat porro?
-      </p>
-    </>
-  );
+	console.log('Oi');
+	return (
+		<>
+			<h1>Olá Mundo!</h1>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam delectus
+				eaque provident, magni harum dolores! Veritatis delectus nam commodi at
+				eligendi tempora voluptatem, eum asperiores sint, consequatur dolorum
+				repellat porro?
+			</p>
+		</>
+	);
 }
